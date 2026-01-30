34db27184dc5a285fe313ea6c6438308416fbd54
Littlestar0508 <rist9860@gmail.com>
2026-01-31 00:26:35 +0900

    FEAT : 랜딩페이지 대략적인 UI 구성

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,12 @@
+export default function Landing() {
+  return (
+    <>
+      <form className="w-3/5 flex gap-20 mx-auto mt-20">
+        <input type="text" className="flex-1 border" />
+        <button type="button" className="bg-primary flex-1">
+          다음
+        </button>
+      </form>
+    </>
+  );
+}
